a043e1cfd0037fbe545fbdc6af4db9dd89dba122
david-aravena <david.aravenap@gmail.com>
2025-01-09 17:24:54 -0400

    add: info cards

--- src/app/(social)/page.jsx
+++ src/app/(social)/page.jsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import Image from "next/image";
 import CardSocialUI from "src/ui/social/CardSocialUI"
 import {getSocialPosts} from 'src/utils/social/'
+import styles from './socialUI.module.css'
+
 
 export default async function Social(){
 
@@ -11,13 +13,15 @@ export default async function Social(){
         <div style={{width:"100%", display:"flex", flexDirection:"column", alignItems:"center"}}>
             {posts.map((item, index) => (
                 <Link href={`/show/${item.id}`} key={index}>
-                    <CardSocialUI item={item} index={index}>
-                        <Image
-                            src={item.image}
-                            alt="error"
-                            layout="fill"
-                            objectFit="cover"
-                        />
+                    <CardSocialUI item={item} index={index} styles={styles}>
+                        <div className={styles.imageContainer}>
+                            <Image
+                                src={item.image}
+                                alt="error"
+                                layout="fill"
+                                objectFit="cover"
+                            />
+                        </div>
                     </CardSocialUI>
                 </Link>
             ))}
--- src/ui/social/CardSocialUI.jsx
+++ src/ui/social/CardSocialUI.jsx
@@ -1,22 +1,21 @@
-import styles from './cardSocialUI.module.css'
 
-export default function CardSocialUI({children, item}){
+export default function CardSocialUI({children, item, styles}){
     return(
         <>
             <div className={styles.card}>
-                <div className={styles.imageContainer}>
-                    {children}
-                </div>
-            
                 <div className={styles.contentContainer}>
-                    <h3>{item.author}</h3>
-                    <p>{item.content}</p>
-                    <div style={{display:"flex", justifyContent:"space-between", padding:"8px"}}>
-                        <button className="buttons">comentarios</button>
-                        <button className="buttons">me interesa</button>
+                    <div style={{display:"flex"}}>
+                        <img src="https://media.istockphoto.com/id/1335941248/es/foto/foto-de-un-joven-guapo-de-pie-sobre-un-fondo-gris.jpg?s=612x612&w=0&k=20&c=UK8BGVVCSmHen0sJAgxoZ6sBLrs0ld4TuCwczZZ89AY=" height={"50px"} width={"50px"} style={{borderRadius:"50px", objectFit:"cover"}} />
+                        <h3>{item.author}</h3>
                     </div>
+                    <p>{item.content}</p>
                 </div>
-                
+
+                    {children}
+            </div>
+            <div style={{display:"flex", justifyContent:"space-between"}}>
+                <button className="buttons">comentarios</button>
+                <button className="buttons">me interesa</button>
             </div>
             
         </>
