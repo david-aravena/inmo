15766f4b5b3af26f495714e0b5edc18c6141f961
david-aravena <david.aravenap@gmail.com>
2025-01-13 22:25:06 -0400

    fix: general styles

--- src/components/changeContentBar/index.jsx
+++ src/components/changeContentBar/index.jsx
@@ -1,18 +1,56 @@
-import Link from 'next/link'
-import Image from 'next/image'
+"use client";
+import Link from 'next/link';
+import Image from 'next/image';
+import { usePathname } from 'next/navigation';
 
-export default function ChangeContentBar(){
-    return(
-        <div style={{background:"#1e1e1e", display:"flex", padding:"4px", borderRadius:"1rem 1rem 0 0"}}>
-            <Link href="/info">
-                <Image src="/svg/info.svg" width={40} height={40} alt="Error logo" style={{padding:"0 8px"}} />
-            </Link>
-            <Link href="/properties">
-                <Image src="/svg/realstate.svg" width={40} height={40} alt="Error logo" style={{padding:"0 8px"}}/>
-            </Link>
-            <Link href="/">
-                <Image src="/svg/users.svg" width={40} height={40} alt="Error logo" style={{padding:"0 8px"}} />
-            </Link>
-        </div>
-    )
+export default function ChangeContentBar() {
+
+    const pathname = usePathname();
+
+    if (pathname.startsWith('/show')) {
+        return null;
+    }
+
+  const isActive = (path) => pathname === path;
+
+  return (
+    <div className="changeContentBar">
+        <Link href="/">
+            <Image
+            src="/svg/users.svg"
+            width={40}
+            height={40}
+            alt="Users logo"
+            style={{
+                padding: "0 8px",
+                filter: isActive('/') ? 'brightness(500%)' : 'none'
+            }}
+            />
+        </Link>
+        <Link href="/properties">
+            <Image
+            src="/svg/realstate.svg"
+            width={40}
+            height={40}
+            alt="Real estate logo"
+            style={{
+                padding: "0 8px",
+                filter: isActive('/properties') ? 'brightness(500%)' : 'none',
+            }}
+            />
+        </Link>
+        <Link href="/info">
+            <Image
+            src="/svg/info.svg"
+            width={40}
+            height={40}
+            alt="Info logo"
+            style={{
+                padding: "0 8px",
+                filter: isActive('/info') ? 'brightness(500%)' : 'none',
+            }}
+            />
+        </Link>
+    </div>
+  );
 }
