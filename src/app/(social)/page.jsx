b7b4ffc50815ce2196322fc882afb1c3a2b34c7a
david-aravena <david.aravenap@gmail.com>
2025-01-20 15:57:38 -0400

    add: icon search item

--- src/app/(social)/page.jsx
+++ src/app/(social)/page.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import CardSocialUI from "src/ui/social/CardSocialUI"
 import ChangeContentBar from 'src/components/changeContentBar/'
+import Search from 'src/components/searchItems/'
 import {getSocialPosts} from 'src/utils/social/'
 import styles from './socialUI.module.css'
 
@@ -10,21 +11,26 @@ export default async function Social(){
     const posts = await getSocialPosts()
 
     return(
-        <div style={{width:"100%", display:"flex", flexDirection:"column", alignItems:"center"}}>
-            {posts.map((item) => (
-                <CardSocialUI item={item} index={item.id} styles={styles}>
-                    <Image
-                        src={item.image}
-                        alt="error"
-                        layout="fill"
-                        style={{objectFit:"cover", height:"100%", width:"100%"}}
+        <>
+            <div style={{position:"relative", width:"100%", display:"flex", flexDirection:"column", alignItems:"center"}}>
+                <div className="searchContainer" style={{width: "fit-content"}}>
+                    <Search />
+                </div>
+                {posts.map((item) => (
+                    <CardSocialUI item={item} index={item.id} styles={styles}>
+                        <Image
+                            src={item.image}
+                            alt="error"
+                            layout="fill"
+                            style={{objectFit:"cover", height:"100%", width:"100%"}}
 
-                    />
-                </CardSocialUI>
-            ))}
+                        />
+                    </CardSocialUI>
+                ))}
+            </div>
             <div className="changeContentBarContainer">
                 <ChangeContentBar />
             </div>
-        </div>
+        </>
     )
 }
---
+++ src/components/searchItems/index.jsx
@@ -0,0 +1,7 @@
+export default function SearchItems(){
+    return(
+        <div style={{display:"flex", fontSize:"1rem", backgroundColor:"#3980ff", padding:"8px", borderRadius:"10rem"}}>
+            <img src="/svg/search.svg" alt="Foto search" />
+        </div>
+    )
+} 
